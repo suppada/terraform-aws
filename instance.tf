6c6124b6a74f7e9c2b2405c349f63b30070dfbf5
Suresh <68796452+suppada@users.noreply.github.com>
2025-03-05 03:50:43 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,27 +1,7 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-// EC2 Instance Resource for Module
-data "aws_ami" "ansible" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
-}
-
 resource "aws_instance" "ec2_instance" {
   count                       = var.ec2_count
   instance_type               = var.instance_type
-  ami                         = data.aws_ami.ansible.id
+  ami                         = var.ami_id
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.sg.id]
   iam_instance_profile        = aws_iam_instance_profile.profile.name
